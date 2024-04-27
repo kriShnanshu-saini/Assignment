0631f3a7cb33db8c1169d6e72e63b9735b940d1c
Harshak Saini <harshaksaini@gmail.com>
2024-04-27 22:24:44 +0530

    tags-filter-feature

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,13 +2,39 @@ import React, { useState, useEffect } from "react";
 import { Table, Input, Tag } from "antd";
 import PaginationComponent from "./PaginationComponent"; // Import PaginationComponent
 // import SearchBar from "./SearchBar";
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from "query-string";
 const { Search } = Input;
 const PostList = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize] = useState(10); // Number of items per page
   const [totalItems, setTotalItems] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+
+  const tagsArr = new Set();
+  const allTags = () => {
+    posts.forEach((post) => {
+      post.tags.map((tag) => tagsArr.add(tag));
+    });
+    console.log([...tagsArr]);
+  };
+
+  useEffect(() => {
+    const queryParams = queryString.parse(location.search);
+
+    // Set current page from query parameters
+    if (queryParams.page) {
+      setCurrentPage(parseInt(queryParams.page));
+    }
+
+    // Set search query from query parameters
+    if (queryParams.q) {
+      setSearchQuery(queryParams.q);
+    }
+  }, [location.search]);
 
   useEffect(() => {
     // Fetch total number of items for pagination
@@ -19,11 +45,9 @@ const PostList = () => {
         const filteredPosts = searchQuery
           ? data.posts.filter((post) =>
               post.body.toLowerCase().includes(searchQuery.toLowerCase())
-            )
-          : setTotalItems(data.total);
-        if (searchQuery) {
-          setTotalItems(filteredPosts ? filteredPosts.length : 0);
-        }
+            ).length
+          : data.total;
+        setTotalItems(filteredPosts);
       });
 
     // Fetch data from API based on current page and page size
@@ -43,6 +67,7 @@ const PostList = () => {
         setPosts(filteredPosts);
       });
   }, [currentPage, pageSize, searchQuery]);
+  allTags();
 
   const columns = [
     { title: "Title", dataIndex: "title", key: "title" },
@@ -58,17 +83,47 @@ const PostList = () => {
           ))}
         </>
       ),
+      filterMode: "tree",
+      filterSearch: true,
+      onFilter: (value, record) => record.tags.indexOf(value) !== -1, // returns boolean value
+      filters: [...tagsArr].map((tag) => {
+        return {
+          text: tag[0].toUpperCase() + tag.substring(1).toLowerCase(),
+          value: tag,
+        };
+      }),
     },
     // Add more columns as needed
   ];
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
+    updateURLParams({ page });
   };
-  const handleSearch = (value) => {
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
     setSearchQuery(value);
-    console.log(value);
-    setCurrentPage(1); // Reset to first page when searching
+    setCurrentPage(1);
+    // Reset to first page when searching
+    console.log(!value);
+    if (!value) {
+      console.log("h");
+      updateURLParams({ page: 1 });
+    } else updateURLParams({ page: 1, q: e.target.value });
+  };
+
+  const updateURLParams = (params) => {
+    console.log(queryString.parse(location.search));
+    console.log(params);
+    const newParams = params.q
+      ? {
+          ...queryString.parse(location.search),
+          ...params,
+        }
+      : params;
+    console.log(newParams);
+    const newQueryString = queryString.stringify(newParams);
+    navigate(`${location.pathname}?${newQueryString}`);
   };
 
   return (
@@ -76,8 +131,7 @@ const PostList = () => {
       <h2>Post List</h2>
       <Search
         placeholder="Search posts"
-        onSearch={handleSearch}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        onChange={handleSearchChange}
         style={{ width: 200, marginBottom: 16 }}
       />
       <Table columns={columns} dataSource={posts} pagination={false} />
